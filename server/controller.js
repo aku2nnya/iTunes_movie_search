30a9f572c0a11cd49b263903710293e623800494
Toshihiro Akabori <toshihiro.akabori@yahoo.com>
2019-10-22 11:02:45 -0700

    Tweak error handling

--- server/controller.js
+++ server/controller.js
@@ -1,11 +1,14 @@
 const axios = require('axios');
 
 
-const getMovieData = (req, res) => {
+function getMovieData(req, res) {
   axios.get(`https://itunes.apple.com/search?entity=movie&term=${req.params.searchTerm}`)
     .then((response) => res.send(response.data))
-    .catch((error) => console.error(error));
-};
+    .catch((error) => {
+      console.error(error);
+      res.send(error);
+    });
+}
 
 
 module.exports = { getMovieData };
