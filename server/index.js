a77a528a26364ade68d6226c89ef0cdb94bc48ea
Toshihiro Akabori <toshihiro.akabori@yahoo.com>
2019-10-21 19:39:44 -0700

    Add public folder to static use

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const { getMovieData } = require('./controller');
 const app = express();
 const PORT = 3000;
 
+app.use(express.static('public'));
+
 app.get('/:searchTerm', getMovieData);
 
 app.listen(PORT, () => console.log(`Listening to port ${PORT}`));
